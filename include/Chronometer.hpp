fb037a4ae00fb31d44b1d9eaafd275a2165477be
SERGIO <alu0101133201@cc1212.etsii.ull.es>
2020-02-10 12:59:31 +0000

    Adición de la clase cronómetro - defectuoso

--- include/Chronometer.hpp
+++ include/Chronometer.hpp
@@ -1,1 +1,29 @@
+/*
+ * Práctica 01 - DAA
+ * Sergio Guerra Arencibia
+ *
+ * Clase que implementa un cronómeto mediante el uso de la librería chrono. Fichero hpp
+ */
 
+#include <iostream>
+#include <chrono>
+#include <ctime>
+
+class Chronometer{
+
+  private:
+
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+    std::chrono::duration<double> elapsed_seconds;
+
+  public:
+
+    Chronometer(void);
+    ~Chronometer(void);
+
+    void start_(void);
+    void stop(void);
+
+    std::chrono::duration<double> get_duration(void);
+
+};
